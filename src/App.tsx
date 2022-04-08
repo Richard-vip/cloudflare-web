e68b7273566a342ccafde4b93667452bf9315605
wangrenjie03 <wangrenjie03@kuaishou.com>
2022-04-08 14:49:30 +0800

    fix: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import axios  from 'axios';
+import axios from 'axios';
 import './App.css';
 
 
 const App = () => {
 
-  const [keys, setKeys] = useState<string[]>([]);
-  const [values, setValues] = useState<string[]>([]);
-
-  const [key, setKey] = useState('');
-  const [value, setValue] = useState('');
+  const [name, setName] = useState('');
 
 
-  const getList = () => {
-    axios.get('https://cloudflare-service.richard-v.workers.dev/list').then(res=>res.data).then(res=>{
-      if (res?.data?.keys?.length) {
-        setKeys(res.data.keys)
-      }
-      if (res?.data?.data?.length) {
-        setValues(res.data.data)
-      }
-    })
-  }
-
-  const handleKeyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
-    setKey(value)
-  }
-
-  const handleValChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
-    setValue(value)
-  }
-
-
-  const addField = () => {
-    axios.post('https://cloudflare-service.richard-v.workers.dev/create',{
-      key,
-      value
+  const getName = () => {
+    axios('https://cloudflare-service.richard-v.workers.dev/get', {
+      method: 'GET',
+      params: { key: 'name' }
     }).then(res => {
-      console.log(res)
-      if (res.status) {
-        getList();
+      if (res?.data?.data) {
+        setName(res.data.data)
       }
     })
   }
 
   useEffect(() => {
-    getList();
+    getName()
   }, [])
 
 
@@ -55,19 +28,9 @@ const App = () => {
   return (
     <div className="App">
       hello word <br />
-      <table>
-        <tbody>
-          {keys.map((key, idx) => <tr>
-            <td>{key}</td>
-            <td>{values[idx]}</td>
-          </tr>)}
-        </tbody>
-      </table>
+      {name}
 
-      <input type="text" name='key' onChange={handleKeyChange} />
-      <input type="text" name='values' onChange={handleValChange} />
-
-      <button onClick={() => addField()}> 添加</button>
+      <button onClick={getName}>获取name</button>
     </div>
   );
 }
